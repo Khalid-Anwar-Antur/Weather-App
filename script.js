d85271bd5906ed04b87908b3f8dd8e03295db74a
Khalid-Anwar-Antur <khalidanwaranturpro@gmail.com>
2025-03-22 23:15:01 +0600

    project done

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 const apiKey = "99085ef3539ec0276cf290e8bb15804d";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhaka";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox = document.querySelector('.inputArea');
+const searchButton = document.querySelector('.search-btn');
+const weatherIcon = document.querySelector('.weather-icon');
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
-    var data = await response.json();
-    console.log(data)    
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    if(response.status==404){
+        document.querySelector('.error').style.display = 'block'
+        document.querySelector('.weather').style.display = 'none'
+    }else{
+
+        var data = await response.json();
+        console.log(data);
+        document.querySelector('.city').innerHTML= data.name;
+        document.querySelector('.temp').innerHTML= Math.round(data.main.temp) + '°C';
+        document.querySelector('.humidity').innerHTML= data.main.humidity+'%';
+        document.querySelector('.wind').innerHTML= data.wind.speed+ ' '+ 'km/h';
+    
+        if(data.weather[0].main =='Clouds'){
+            weatherIcon.src = 'images/clouds.png'
+        }
+        else if(data.weather[0].main =='Clear'){
+            weatherIcon.src = 'images/clear.png'
+        }
+        else if(data.weather[0].main =='Rain'){
+            weatherIcon.src = 'images/rain.png'
+        }
+        else if(data.weather[0].main =='Drizzle'){
+            weatherIcon.src = 'images/drizzle.png'
+        }
+        else if(data.weather[0].main =='Mist'){
+            weatherIcon.src = 'images/mist.png'
+        }
+        
+        document.querySelector('.weather').style.display = 'block'   
+        document.querySelector('.error').style.display = 'none'   
+    }
+
+
+
 }
-checkWeather()
+
+searchButton.addEventListener('click',()=>{
+    checkWeather(searchBox.value);
+})
